3223db569c89523bec2d8f0093ebf3b4b513f612
jpal29 <jpal.lee7@gmail.com>
2018-10-28 19:20:34 -0700

    Delete create_docker_db.sh

--- create_docker_db.sh
+++
@@ -1,6 +0,0 @@
-#Want to use this for testing the creation of mysql databases in a docker container
-
-docker run --name mysql -d -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
-    -e MYSQL_DATABASE=eplApi -e MYSQL_USER=flaskuser \
-    -e MYSQL_PASSWORD=Bun6#0le \
-    mysql/mysql-server:5.7
